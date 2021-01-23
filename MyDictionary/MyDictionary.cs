0537fc2f0c3b8b3ea1cc500dabcebcba0ba6e5a5
yigit <yigit.nebioglu@hotmail.com>
2021-01-23 20:53:53 +0300

    a

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TKey[] tempKeys;
+        TValue[] values;
+        TValue[] tempValues;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                if (key == null)
+                {
+                    throw new ArgumentNullException("Key null olamaz!");
+                }
+                else if (tempKeys[i].Equals(key))
+                {
+                    throw new ArgumentException("Bu Key'e sahip değer mevcut.");
+                }
+            }
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[keys.Length - 1] = value;
+        }
+
+        public void ListAll()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Key: "+keys[i] + " Value: " +values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDict = new MyDictionary<int, string>();
+            myDict.Add(1, "Yiğit");
+            myDict.Add(2, "Metin");
+            myDict.Add(3, "Nuray");
+            myDict.Add(4, "Cemile");
+
+            myDict.ListAll();
+
+        }
+    }
+}
